b2289198474be73ee2a5bd66b2e8495cde511ea8
sasuka9985 <sasuka9985@gmail.com>
2017-05-18 20:21:54 +0800

    done

--- myapp.js
+++ myapp.js
@@ -19,11 +19,11 @@ var skycons = new Skycons();
 /*
 Get value from Bootstrap dropdown menu
 */
-var area = ["Taipei City", "New Taipei City", "Taichung City", "Tainan City", "Kaohsiung City", "Keelung City", "Taoyuan City", "Hsinchu City", "Hsinchu County", "Miaoli County", "Changhua County", "Nantou County", "Yunlin County", "Chiayi City", "Chiayi County", "Pingtung County", "Yilan County", "Hualien County", "Taitung County", "Penghu County", "Kinmen County", "Matsu"];
+var city = ["Taipei City", "New Taipei City", "Taichung City", "Tainan City", "Kaohsiung City", "Keelung City", "Taoyuan City", "Hsinchu City", "Hsinchu County", "Miaoli County", "Changhua County", "Nantou County", "Yunlin County", "Chiayi City", "Chiayi County", "Pingtung County", "Yilan County", "Hualien County", "Taitung County", "Penghu County", "Kinmen County", "Matsu"];
 
 //華氏轉攝氏，四捨五入整數
-function fToCel(fahrenheit) {
-    var x = Math.round((5 / 9) * (fahrenheit - 32))
+function fToCel(f) {
+    var x = Math.round((5 / 9) * (f - 32))
     return x;
 }
 //天氣setIcon
@@ -35,23 +35,23 @@ function setIcon(des) {
         case "Mostly Sunny":
             return "clear_day";
             break;
+        case "Cloudy":
+            return "cloudy";
+            break;
         case "Partly Cloudy":
             return "partly_cloudy_day";
             break;
         case "Mostly Cloudy":
             return "cloudy";
             break;
-        case "Cloudy":
-            return "cloudy";
+        case "Fog":
+            return "fog";
             break;
         case "Windy":
             return "wind";
             break;
         case "Breezy":
             return "wind";
-            break;
-        case "Fog":
-            return "fog";
             break;
         default:
             return "rain";
@@ -75,50 +75,50 @@ function getWeatherJson(url, callback) {
 $(document).ready(function(){
     getWeatherJson("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Taipei%20City%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
         function(data){
-            var currentTemperature = fToCel(data.query.results.channel.item.condition.temp);
-            $('#nowTemp').text(currentTemperature);
-            updatePage(0);
+            pageNow(0);
+            var nowTemp = fToCel(data.query.results.channel.item.condition.temp);
+            $('#nowTemp').text(nowTemp);
         })
 });
 
 //更新頁面
-function updatePage(i) {
-    getWeatherJson("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + area[i] + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+function pageNow(i) {
+    getWeatherJson("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city[i]+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
         function(data) {
             console.log(data);
             $('.temperature').text(fToCel(data.query.results.channel.item.condition.temp));
             $('.date').text(data.query.results.channel.item.forecast[0].date);
-            var des = data.query.results.channel.item.forecast[0].text;
-            $('#condition').text(des);
-            skycons.set("today", setIcon(des));
-            $('.time').each(function(index, element) {
-                var $date = $(element);
-                $date.text(data.query.results.channel.item.forecast[index + 1].date);
+            $('.temp').each(function(i,e) {
+                var temp = $(e);
+                temp.text(fToCel(data.query.results.channel.item.forecast[i+1].high)+"~"+fToCel(data.query.results.channel.item.forecast[i + 1].low));
             })
-            $('.temp').each(function(index, element) {
-                var $range = $(element);
-                $range.text(fToCel(data.query.results.channel.item.forecast[index + 1].high) + " - " + fToCel(data.query.results.channel.item.forecast[index + 1].low));
-            })
+            $('#condition').text(date);
+            skycons.set("today", setIcon(date));
             skycons.set("day1", setIcon(data.query.results.channel.item.forecast[1].text));
             skycons.set("day2", setIcon(data.query.results.channel.item.forecast[2].text));
             skycons.set("day3", setIcon(data.query.results.channel.item.forecast[3].text));
+            var date = data.query.results.channel.item.forecast[0].text;
+            $('.time').each(function(i, e) {
+                var time = $(e);
+                time.text(data.query.results.channel.item.forecast[i+1].date);
+            })
         });
 }
 
 //點選更新頁面，拿城市
 $('#dropdown li').on('click', function() {
-    updatePage($(this).find('a').attr('tabindex'));
+    pageNow($(this).find('a').attr('tabindex'));
     $('#selArea').html($(this).find('a').html());
 });
 
 //選單溫度
 var temps = $('#dropdown li span');
-temps.each(function(i, element) {
+temps.each(function(i, e) {
     var temp = $(this);
-    getWeatherJson("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + area[i] + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+    getWeatherJson("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city[i]+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
         function(data) {
-            var currentTemperature = fToCel(data.query.results.channel.item.condition.temp);
-            temp.text(currentTemperature);
+            var nowTemp = fToCel(data.query.results.channel.item.condition.temp);
+            temp.text(nowTemp);
         });
 })
 
